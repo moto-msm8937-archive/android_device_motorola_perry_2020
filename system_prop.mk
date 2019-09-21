ea28777f74b4f503727ab600a3e6d190bf4b40d8
flagersgit <flagersmail@gmail.com>
2019-09-20 21:19:59 -0400

    montana: decommonize display props

--- system_prop.mk
+++ system_prop.mk
@@ -23,3 +23,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.sensors=montana
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_hwc_vds=1 \
+    debug.egl.hw=0 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    ro.sf.lcd_density=420 \
+    persist.debug.wfd.enable=1 \
+    sdm.debug.disable_skip_validate=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    persist.hwc.enable_vds=1 \
+    debug.sf.recomputecrop=0
